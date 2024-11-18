4b170f1e9be47096a8952372d25c1045f29a35a6
Muhammad Rayyan <154331886+MuhammadRayyan16@users.noreply.github.com>
2024-11-18 14:04:10 +0500

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import '../components/Table.css';
 
 
-
 function Tablegenerator() {
     let [num, setNum] = useState('');
     let [loading, setLoading] = useState(true);
